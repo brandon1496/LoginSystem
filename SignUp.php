39c646f2045b2eecb172a576092e290c7886b0d8
Brandon Innis <brandoninnis96@gmail.com>
2018-12-21 17:37:19 -0500

    Created Sign up page

---
+++ SignUp.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
+    
+</head>
+<body>
+<?php include "Templates/nav.php"; ?>
+<div class="Signup">
+    
+    <form class>
+    <h1>Sign Up</h1>
+        <input name="FirstName" type="Text" placeholder="FirstName">
+        <input name="LastName" type="Text" placeholder="LastName">
+        <input name="Email" type="email" placeholder="Email">
+        <input name="Username" type="Text" placeholder="Username">
+        <input name="Password" type="Text" placeholder="Password">
+        <button>Already Have A account click here</button>
+        <input type="submit" name="Submit">
+    </form>
+</div>
+</body>
+</html>
---
+++ Templates/nav.php
@@ -0,0 +1,7 @@
+<div class="header">
+    <nav class="Nav">
+        <a id="Home" href="#">Home</a>
+        <a id="Login" href="#">Log In</a>
+        <a id="SignUp" href="#">Sign Up</a>
+    </nav>
+</div>
