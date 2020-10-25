8cedd42e663f90435cafdaae08e741918806a786
Brandon Innis <brandoninnis96@gmail.com>
2020-10-25 19:54:02 -0400

    refactored previous deleted files from the last commit

---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<div class="header">
+    <nav class="Nav">
+        <a id="Home" href="index.php">Home</a>
+        <a id="Login" href="index.php">Log In</a>
+        <a id="SignUp" href="signup.php">Sign Up</a>
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -1,13 +1,26 @@
-<?php include "Templates/header.php"; ?>
-<?php include "Templates/nav.php"; ?>
-<?php session_start() ?>
-<div class="Home">
-<h1>Home</h1>
-<?php if (isset($_SESSION["Username"])){
-    $name = $_SESSION["Username"];
-    echo "<p> Hello '$name'</p>";
-}
-?>
+<?php include "includes/header.php";?>
+<div class="row justify-content-center">
+    <div class="col-md-4 col-xs-12">
+        <h3>Login</h3>
+        <p>Please fill this form to sign in.</p>
+        <form>
+            <div class="row justify-content-center">
+                <div class="form-group col-12">
+                    <label>Email</label>
+                    <input type="email" name="Email" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="form-group col-12">
+                    <label >Password</label>
+                    <input type="password" name="Password" class="form-control" placeholder="Password">
+                </div>
+            </div>
+            <span class="btns">
+                <button type="submit" class="btn btn-primary">Sign In</button>
+                <a class="btn btn-info" href="#" role="button">Forget Password</a>
+            </span>
+        </form>
+    </div>
 </div>
-</body>
-</html>
+<?php include "includes/footer.php";?>
